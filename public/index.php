e24964d83e7a6cbe4b2dbc1585e286bbb3548eb7
Badara <bbabadara@gmail.com>
2026-05-06 14:19:45 +0000

    refactor index

--- public/index.php
+++ public/index.php
@@ -2,28 +2,22 @@
 define("WEBROOT","http://localhost:8002/");
 define("ROOT", (substr($_SERVER['DOCUMENT_ROOT'] ,0, -6)));
 
-// var_dump(jsonToArray("classes"));
-// die;
 require_once(ROOT."views/header.php");
 
 
 $view=ROOT."views/classes";
+$controllers=[
+    "etudiant"=>"etudiant",
+    "classe"=>"classe"
+];
 
-
- $controller="etudiant";
- if (isset($_REQUEST["controller"])) {
-    $controller=$_REQUEST["controller"];
- }
- if ($controller=="etudiant") {
-   require_once(ROOT."/controller/etudiantController.php");
-
- }
- elseif ($controller=="classe") {
-    require_once(ROOT."/controller/classeController.php");
-
- }else{
+ $controller=$_REQUEST["controller"]??"etudiant";
+if (array_key_exists($controller, $controllers)) {
+    require_once(ROOT."controller/".$controllers[$controller]."Controller.php");
+}
+else{
     echo "controller introuvable";
- }
-
+}
+ 
 
 require_once(ROOT."views/footer.php");
